8b299d61c66348ea9c33914d8b4c7a95437b5e4d
indekx <indekxng@gmail.com>
2019-07-26 10:24:15 +0100

    Production settings updated

--- naturessigns/settings/production.py
+++ naturessigns/settings/production.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('NS_SECRET_KEY')
+SECRET_KEY = 'h%8uhS4if5tjzqdj_+s+!uih-21^y63s9)8i#c$8g9*#jujvyx68jh'
 GOOGLE_RECAPTCHA_SECRET_KEY = os.environ.get('GOOGLE_RECAPTCHA_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
